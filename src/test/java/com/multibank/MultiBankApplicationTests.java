e21e274f5db897e6bc16ec1878be0efe84c6a8db
DariusCotrau <87205413+DariusCotrau@users.noreply.github.com>
2025-09-30 21:07:28 +0300

    Move MultiBankApplication to root package

--- src/test/java/com/multibank/MultiBankApplicationTests.java
+++ src/test/java/com/multibank/MultiBankApplicationTests.java
@@ -1,6 +1,6 @@
 package com.multibank;
 
-import com.multibank.application.MultiBankApplication;
+import com.multibank.MultiBankApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
